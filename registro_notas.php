b51c780120109b8d4b7ad7317668008342a971c7
Totilla <tsm.antonia@gmail.com>
2025-05-21 09:29:54 +0200

    registro notas terminado

--- registro_notas.php
+++ registro_notas.php
@@ -12,9 +12,42 @@
     "MariSol" => ["Matemáticas" => 10, "Historia" => 10, "PHP" => 10]
  ];
 
- //para redondear 
- $nota = 6.78;
- $notaRedondeada = round($nota, 1);
- echo $notaRedondeada;
+echo '<p>Notas de los alumnos / alumnas</p>';
+echo '<table border="1">';
+// echo '<tr>
+//       <th>Nombre</th>
+//       <th>Matemáticas</th>
+//       <th>Historia</th>
+//       <th>PHP</th>
+//       <th>Media</th>
+//       </tr>';
+      $ponerCabecera = true;
+      foreach($alumnos as $nombre => $datos){
+         //$datos es un array asociativo con las asignaturas y las notas
+         //este if se ejecuta solo una vez
+         if($ponerCabecera){
+            echo '<tr>';
+            echo '<th>Nombre</th>';
+            foreach($datos as $nombreAsignatura => $notaAsignatura){
+               echo '<th>'. $nombreAsignatura.'</th>';
+            }
+            echo '<th>Media</th>';
+            echo '</tr>';
+            $ponerCabecera = false;
+         }
+         //en notaMedia voy acumulando el valor de las notas
+         $notaMedia = 0;
+
+         echo '<tr>';
+         echo "<td>$nombre</td>";
+         foreach($datos as $nombreAsignatura => $notaAsignatura){
+            echo "<td>$notaAsignatura</td>";
+            $notaMedia += $notaAsignatura;
+         }
+         echo "<td>" . number_format(round($notaMedia / count($datos),2),2, ",", ".") . "</td>";
+         echo '</tr>';
+      }
+echo '</table>';
+
 
 
